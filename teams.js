a2df0ef814527d053d9bbc016cbc0a520e26dba7
André Alcalde <andrealcalde@Air-de-Andre.lan>
2021-07-17 13:58:45 +0200

    Primer commit (array equipos)

---
+++ teams.js
@@ -0,0 +1,18 @@
+export const teams = ['España',
+    'Portugal',
+    'Francia',
+    'Inglaterra',
+    'Italia',
+    'Dinamarca',
+    'Suecia',
+    'Suiza',
+    'Holanda',
+    'Turquía',
+    'Grecia',
+    'Polonia',
+    'Alemania',
+    'Eslovenia',
+    'Ucrania',
+    'Bélgica'];
+
+
